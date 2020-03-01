4bf8b1f06bc56b2faf314ae3a1ed1cb940e7cebb
Aleksandra Nikiciuk <o.szumarska@gmail.com>
2020-03-02 00:41:06 +0100

    stlesheet added

--- js/app.js
+++ js/app.js
@@ -1,47 +1,70 @@
     const startButton = document.getElementById("start");
-    const fibonacciList = document.querySelector(".fibonacci_List")
+    const resetButton = document.getElementById("reset");
+    const fibonacciList = document.querySelector(".fibonacci__list");
+    const userNumber = document.getElementById("fnumber");
+    const prompt = document.getElementById("prompt");
 
-    //Creating array with Fibonacci sequence 
-    function fibonacciNumbers(){
-        let sequenceLength = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;   //generate a random number between 1 - 1000
+    
+    //Creating array with Fibonacci sequence
+    function createFibonacciNumbers(){
+        let fibonacciLength = userNumber.value - 1;
         let fibonacciNumbers = [];
         
-        for (let i=0; i<=sequenceLength; i++) {
-            if (i === 0 ) { fibonacciNumbers.push(0)};
-            if (i === 1) { fibonacciNumbers.push(1)};
+        for (let i=0; i<=fibonacciLength; i++){
+            if (i === 0) {fibonacciNumbers.push(0)};
+            if (i === 1) {fibonacciNumbers.push(1)};
             if (i >= 2) {fibonacciNumbers.push(fibonacciNumbers[i-1] + fibonacciNumbers[i-2])};
         }
-
         return fibonacciNumbers
     }
 
     //Maping Fibonacci numbers into next HTML rows
-    function showFibonacciNumbers(numbers) {
-        const fragment = document.createDocumentFragment();
-        
+    function showFibonacciNumbers(numbers){
+        const fragment = document.createDocumentFragment();     
+
         numbers.map(function(e,i){
-            let listItem = document.createElement("h3");
+            let listItem = document.createElement("li");
             listItem.innerText = e;
   
             listItem.style.animationDelay = i * 3 + "s"; // displaying next numbers evry 3s
             fragment.appendChild(listItem)   
-        })
-    
-        fibonacciList.appendChild(fragment)  
-        
+        })   
+
+        fibonacciList.appendChild(fragment)          
     }
 
-    //Deleting previously generated sequence  
-    function deleteSequence() {
-        
-        fibonacciList.querySelectorAll("h3") ?  fibonacciList.textContent = '' : fibonacciList         
+    //Input validating function
+    function validateForm(){
+        let number = parseFloat(userNumber.value, 10);
+
+        if ( number<=0 || Number.isInteger(number) === false){
+            prompt.innerHTML = "Podano niewłaściwą wartość. Podana wartość powinna być dodatnią liczbą całkowitą";        
+            prompt.classList.add("alert");
+            return false;
+        } else {
+            prompt.innerHTML = `Liczę ${number} wyraz ciągu Fibonacciego`;
+            startButton.disabled="disabled";
+            prompt.classList.remove("alert");
+            return true;
+        }
+    }
+
+    function resetFibonacciNumbers(){
+        startButton.disabled=false;
+        fibonacciList.querySelectorAll("li") ?  fibonacciList.textContent = '' : fibonacciList;
+        prompt.innerHTML = "";
+        userNumber.value = "";
     }
 
     //Generating new Fibonacci sequence
-    function getFibonacciNumbers() {
-        deleteSequence()
-        showFibonacciNumbers(fibonacciNumbers())
+    function getFibonacciNumbers(){      
+        if (validateForm()){
+            let a = createFibonacciNumbers();
+            showFibonacciNumbers(a);
+        } else {
+            return
+        };
     }
 
-    
     startButton.addEventListener("click", getFibonacciNumbers);
+    resetButton.addEventListener("click", resetFibonacciNumbers);
